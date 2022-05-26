3ad4174d2fc4885c99afc7ef29a0838120fb0d16
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2022-05-26 17:43:29 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Aciklama = "Amasya Elması";
+            urun1.Fiyati = 15;
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+            urun2.Fiyati = 80;
+            //instance
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+        }
+    }
+}
+
+
+//dont repeat yourself - DRY - Clean Code - Best Practice
